dc5ba5c15cff41d8e29329ec72f7cd83717da823
nhocchoda <nhocchoda@gmail.com>
2019-04-01 18:09:14 +0700

    add click listener lý thuyết và ôn thi

--- app/src/main/java/com/spark/cong/ic3spark/MainActivity.java
+++ app/src/main/java/com/spark/cong/ic3spark/MainActivity.java
@@ -2,12 +2,28 @@ package com.spark.cong.ic3spark;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    FrameLayout fl_lythuyet,fl_onthi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fl_lythuyet=(FrameLayout) findViewById(R.id.frameLayout_LyThuyet);
+        fl_onthi=(FrameLayout)findViewById(R.id.frameLayout_Onthi);
+
+        fl_lythuyet.setOnClickListener(mOnclickListener);
+        fl_onthi.setOnClickListener(mOnclickListener);
     }
+    View.OnClickListener mOnclickListener= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),"ok",Toast.LENGTH_SHORT).show();
+        }
+    };
+
 }
