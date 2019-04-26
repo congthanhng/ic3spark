4993490666ecd7f338b6972bf4f0e6f629402be1
nhocchoda <nhocchoda@gmail.com>
2019-04-26 14:49:56 +0700

    add demo PDF viewer

--- app/src/main/java/com/spark/cong/ic3spark/LyThuyet/LythuyetActivity.java
+++ app/src/main/java/com/spark/cong/ic3spark/LyThuyet/LythuyetActivity.java
@@ -3,13 +3,17 @@ package com.spark.cong.ic3spark.LyThuyet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
 import com.spark.cong.ic3spark.R;
 
 public class LythuyetActivity extends AppCompatActivity {
-
+    PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lythuyet);
+        pdfView=(PDFView)findViewById(R.id.pdfViewer);
+
+        pdfView.fromAsset("demo.pdf").load();
     }
 }
