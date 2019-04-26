effc6b743dbb040fca59da4fb213638589c90e56
nhocchoda <nhocchoda@gmail.com>
2019-04-26 13:34:07 +0700

    event click image button ly thuyet va on tap

--- app/src/main/java/com/spark/cong/ic3spark/MainActivity.java
+++ app/src/main/java/com/spark/cong/ic3spark/MainActivity.java
@@ -1,28 +1,33 @@
 package com.spark.cong.ic3spark;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    FrameLayout fl_lythuyet,fl_onthi;
+    ImageButton imgButton_lythuyet,imgButton_onthi;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fl_lythuyet=(FrameLayout) findViewById(R.id.frameLayout_LyThuyet);
-        fl_onthi=(FrameLayout)findViewById(R.id.frameLayout_Onthi);
-
-        fl_lythuyet.setOnClickListener(mOnclickListener);
-        fl_onthi.setOnClickListener(mOnclickListener);
+        imgButton_lythuyet=(ImageButton) findViewById(R.id.imageButton_LyThuyet);
+        imgButton_onthi=(ImageButton)findViewById(R.id.imageButton_OnThi);
+        button=(Button)findViewById(R.id.buttonok);
+        button.setOnClickListener(mOnclickListener);
+        imgButton_lythuyet.setOnClickListener(mOnclickListener);
+        imgButton_onthi.setOnClickListener(mOnclickListener);
     }
     View.OnClickListener mOnclickListener= new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getApplicationContext(),"ok",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext() ,"ok" ,Toast.LENGTH_SHORT).show();
         }
     };
 
